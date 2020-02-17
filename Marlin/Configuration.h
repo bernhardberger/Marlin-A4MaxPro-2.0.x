175aeb2b323ba772651d1e8e99b029f7e0468b67
Bernhard Berger <bernhard.berger@gmail.com>
2020-02-17 17:18:01 +0100

    set autotune pid values for the bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -539,13 +539,12 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   //Anycubic i3 Mega Ultrabase (0.9Ω @ 22°C)
-  #define DEFAULT_bedKp 439.45
-  #define DEFAULT_bedKi 73.49
-  #define DEFAULT_bedKd 656.97
+//  #define DEFAULT_bedKp 180.03
+//  #define DEFAULT_bedKi 27.02
+//  #define DEFAULT_bedKd 510.76
+
   //Anycubic 4Max Pro Ultrabase (0.9Ω @ 22°C)
   #define DEFAULT_bedKp 143.88
-  //Anycubic i3 Mega Ultrabase (0.9Ω @ 22°C)
-  #define DEFAULT_bedKp 180.03
   #define DEFAULT_bedKi 34.88
   #define DEFAULT_bedKd 619.44
 
